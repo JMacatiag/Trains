f1c9ad7498e3a950926611b3f2eac1a0d3141763
Jmactiag <jessie.macatiag@gmail.com>
2018-01-12 16:51:33 -0800

    add time calculations

--- assets/javascript.js
+++ assets/javascript.js
@@ -27,8 +27,9 @@ $("#add-train-btn").on("click", function(event) {
     trainName: trainName,
     destination: destination,
     firstTrain: firstTrain,
-    frequency: frequency
+    frequency: frequency,
   };
+
     // Push to firebase
   database.ref().push(newTrain);
 
@@ -37,3 +38,36 @@ $("#add-train-btn").on("click", function(event) {
   console.log(newTrain.destination);
   console.log(newTrain.firstTrain);
   console.log(newTrain.frequency);
+
+    // Clears all of the text-boxes
+  $("#trainNameInput").val("");
+  $("#destinationInput").val("");
+  $("#firstTrainInput").val("");
+  $("#frequencynput").val("");
+
+  // Time calculations
+  var trainFrequency=frequency;
+  var firstTrainTime=firstTrain;
+
+  // First Time
+  var firstTrainTimeConverted=moment(firstTrainTime, "hh:mm").subtract(1, "years");
+
+  // current time
+  var currentTime=moment();
+
+  // time difference
+  var timeDifference = moment().diff(moment(firstTrainTimeConverted), "minutes");
+
+  // time apart
+  var timeApart=timeDifference%trainFrequency;
+
+  // minutes until next train
+  var minutesToNextTrain=trainfrequency-timeApart;
+
+  // next train
+  var nextTrain=moment().add(tMinutesTillTrain, "minutes");
+
+
+
+
+});
